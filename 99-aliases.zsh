dee36a93862d3aedc18fec47d18097762adc3e76
Marnu Lombard <marnu@mdstech.co.za>
2017-01-16 16:38:55 +0200

    Add aliases for Composer, Laravel, PsySh, K, and a xClip->pbcopy mapping

--- 99-aliases.zsh
+++ 99-aliases.zsh
@@ -5,4 +5,41 @@
 # For a full list of active aliases, run `alias`.
 
 # https://github.com/sindresorhus/trash-cli
-alias -g rm=trash
+alias rm=trash
+
+# Composer alias
+alias cda='composer dumpautoload'
+
+# Laravel's cli
+alias artisan='php artisan'
+
+# Laravel's REPL / Psysh 
+tinker() {
+	if [[ -f ./artisan ]]; then
+		php artisan tinker
+	else
+		psysh 
+	fi
+} 
+
+# K is the new L
+alias ll='k -ah --no-vcs'
+
+# Easier grep
+alias -g G='| grep -i '
+
+# As grabbed from Bash-it - https://github.com/Bash-it/bash-it/
+# to use it just install xclip on your distribution and it would work like:
+# $ echo "hello" | pbcopy
+# $ pbpaste
+# hello
+
+# very useful for things like:
+# cat ~/.ssh/id_rsa.pub | pbcopy
+case $OSTYPE in
+  linux*)
+    XCLIP=$(command -v xclip)
+    [[ $XCLIP ]] && alias pbcopy="$XCLIP -selection clipboard" && alias pbpaste="$XCLIP -selection clipboard -o"
+    ;;   
+esac
+
