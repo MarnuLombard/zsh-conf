3c0f5fb1dd85db0281dc4483eb108e532db80c43
Marnu Lombard <marnulombard@gmail.com>
2020-11-09 08:22:51 +0200

    Update history config for ZSH

--- 01-zsh-config.zsh
+++ 01-zsh-config.zsh
@@ -38,6 +38,20 @@ export COMPLETION_WAITING_DOTS="true"
 # stamp shown in the history command output.
 # The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
 # HIST_STAMPS="mm/dd/yyyy"
+HISTSIZE=$(echo "$HISTSIZE * 3" | bc -l) #tripple the default history size
+SAVEHIST=$(echo "$SAVEHIST * 5" | bc -l) #5 times the default size
+
+export HISTSIZE;
+export SAVEHIST;
+
+unsetopt EXTENDED_HISTORY          # Write the history file in the ":start:elapsed;command" format.
+
+setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+setopt SHARE_HISTORY             # Share history between all sessions.
+setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
+setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
+setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
 
 # Would you like to use another custom folder than $ZSH/custom?
 # ZSH_CUSTOM=/path/to/new-custom-folder
